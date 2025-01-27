be8f5ec06f1f8c9688f33b23897be9c0ab9bb11c
Madhura Tayade <tayademadhura02@gmail.com>
2025-01-27 22:30:21 +0530

    added navbar and started DisplayHome component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
+import Display from "./components/Display";
 
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="flex h-[90%]">
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => {
+            return (
+              <AlbumItem
+                key={index}
+                image={item.image}
+                name={item.name}
+                desc={item.desc}
+                id={item.id}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
